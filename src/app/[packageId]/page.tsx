f62065c1452acf648ad6dc53a7d4a2715b2b81f2
Guillermo Tala <guille86598@gmail.com>
2023-07-19 11:08:39 +0200

    fix metadata details

--- src/app/[packageId]/page.tsx
+++ src/app/[packageId]/page.tsx
@@ -10,6 +10,9 @@ export default function Details({ params }: { params: { packageId: string } }) {
   const { address } = SuiWalletConnect();
   const [metadata, setMetadata] = useState<any[]>([]);
 
+
+  console.log("metadata", metadata)
+
   async function getAsyncObject() {
     const data = await getObjectById(address as string, params.packageId);
     setMetadata(data);
@@ -26,33 +29,30 @@ export default function Details({ params }: { params: { packageId: string } }) {
     <div className="flex min-h-screen flex-col items-center justify-between p-24">
         <div className="w-full max-w-6xl items-start justify-between font-mono text-sm lg:flex lg:py-20">
           <div className="w-1/3 bg-black lg:relative">
-            {metadata[0]?.content?.fields.image_url &&
+            {metadata[0]?.content?.fields.url &&
                    <img
-                   src={
-                     "https://ipfs.io/ipfs/" +
-                     metadata[0].content?.fields.image_url.replace("ipfs://", "")
-                   }
+                   src={metadata[0].content?.fields.url}
                    alt="Nft Img"
                    className="rounded-xl border-[5px] fit-cover min-h-[330px]"
                  />
             }
-     
+
           </div>
 
           <div className="w-2/3 mx-20">
-            <h2 className="text-3xl">{metadataTest.fields.name} #1</h2>
-            <h4 className="text-lg mt-4 mb-8">{metadataTest.fields.description}</h4>
+            <h2 className="text-3xl">{metadata[0]?.content.fields.name} #1</h2>
+            <h4 className="text-lg mt-4 mb-8">{metadata[0]?.content.fields.description}</h4>
             <div className="border-[0.5px] border-gray-600 rounded-md p-4">
               <p className="py-4">Traits:</p>
               <div className="flex flex-wrap -mx-2">
-                {metadataTest.fields.attributes_keys.map(
+                {metadata[0]?.content.fields.attributes_keys.map(
                   (key: string, index: number) => (
                     <div
                       key={index}
                       className="flex flex-col my-2 w-full lg:w-[30%] sm:w-full justify-center border-b border-gray-300 bg-[#232424] dark:border-neutral-800 dark:bg-zinc-800/30 rounded-xl border bg-gray-200 lg:p-4 mx-2"
                     >
                       <p className="text-sm text-gray-600">{key}:</p>
-                      <p>{metadataTest.fields.attributes_values[index]}</p>
+                      <p>{metadata[0]?.content.fields.attributes_values[index]}</p>
                     </div>
                   )
                 )}
